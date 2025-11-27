4d7d60d1464105a9e00bd333eded43d8b03d8b8c
19thMarch <nvtqx1@gmail.com>
2025-11-27 16:56:04 +0700

    Trung_FE_TC

---
+++ Scripts/Frontend/Favourite/TC36_Correct_favourite_for_users/Script1764173824694.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://localhost:8080/login')
+
+WebUI.setText(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/input_Email_email'), '1@test.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/input_Password_password'), 'aeHFOx8jV/A=')
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/button_Sign in'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/svg_Add to Cart_svg-inline--fa fa-heart tra_cabfdd'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/a_Favorites'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/h3_Classic Margherita Pizza'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/button_Logout'))
+
+WebUI.delay(2)
+
+WebUI.setText(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/input_Email_email'), '2@test.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/input_Password_password'), 'aeHFOx8jV/A=')
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/button_Sign in'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/a_Favorites'))
+
+WebUI.click(findTestObject('Object Repository/Frontend/Favourite/TC36/Page_Vite  React/h3_Classic Margherita Pizza'))
+
+WebUI.verifyElementNotPresent(findTestObject('Frontend/Favourite/TC36/Page_Vite  React/h3_Classic Margherita Pizza'), 5)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/TS_DarkMode.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_DarkMode</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f0716643-3b9a-4c84-ae3f-af3a15078835</testSuiteGuid>
+</TestSuiteEntity>
